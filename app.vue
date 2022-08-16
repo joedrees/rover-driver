c401cd9edb6353cad51891ee53ed294aa37e033f
Joe Rees <joedrees@gmail.com>
2022-08-16 22:01:40 +0100

    initial commit, nuxt app with bulma

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="columns">
+    <div id="map" class="column">
+      Map
+      </div>
+      <div id="control" class="column">
+        Control
+      </div>
+    
+  </div>
+</template>
+
+<script>
+
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    css: [
+        // Load a Node.js module directly (here it's a Sass file)
+        'bulma',
+    ]
+})
